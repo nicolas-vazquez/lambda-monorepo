61987cd4406fee57d8ff372d34d8058878019168
VAZQUEZ <nicolasvazquez@iMac.local>
2020-11-02 10:18:19 -0300

    Deploy your AWS Lambda functions based on the files changed in a mono repo

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,8 @@ ZIP_PARAMS=$3
 
 if [ -n "$PATH_NAME" ]; then cd $PATH_NAME; fi
 
-zip lambda.zip -r $ZIP_PARAMS
+npm install
 
-aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://lambda.zip
-aws lambda update-function-configuration --function-name $FUNCTION_NAME
-
-rm -f lambda.zip
+sls deploy -s dev
 
 exit 0
